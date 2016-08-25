3c49fe68fca5ded7d6a0e80d0546efde17442bf5
Kjetil Valle <kjetil.valle@gmail.com>
2016-08-25 22:21:22 +0200

    Added background music theme. Needs cleanup.

--- space-ship-8x32-with-max7219/space-ship-8x32-with-max7219.ino
+++ space-ship-8x32-with-max7219/space-ship-8x32-with-max7219.ino
@@ -29,6 +29,7 @@
 
 // button pins
 #define BUZZER_PIN 5
+#define BUZZER_PIN2 9
 #define FIRE_BUTTON_PIN 6
 #define DOWN_BUTTON_PIN 7
 #define UP_BUTTON_PIN 8
@@ -38,8 +39,8 @@
 
 #define MAX_ASTEROIDS_PER_ROW 10
 
-// enable for debug mode
-#define DEBUG true
+#define DEBUG false
+#define MUSIC_ENABLED false
 
 
 /*
@@ -68,6 +69,311 @@ int game_running = false;
 long screen_buffer[8];
 
 long score = 0;
+
+/*
+   Music stuff
+*/
+
+
+static const byte SILENT = 0;
+static const byte NOTE_C0 = 1;
+static const byte NOTE_DB0 = 2;
+static const byte NOTE_CS0 = 3;
+static const byte NOTE_D0 = 4;
+static const byte NOTE_EB0 = 5;
+static const byte NOTE_DS0 = 6;
+static const byte NOTE_E0 = 7;
+static const byte NOTE_F0 = 8;
+static const byte NOTE_GB0 = 9;
+static const byte NOTE_FS0 = 10;
+static const byte NOTE_G0 = 11;
+static const byte NOTE_AB0 = 12;
+static const byte NOTE_GS0 = 13;
+static const byte NOTE_A0 = 14;
+static const byte NOTE_BB0 = 15;
+static const byte NOTE_AS0 = 16;
+static const byte NOTE_B0 = 17;
+static const byte NOTE_C1 = 18;
+static const byte NOTE_DB1 = 19;
+static const byte NOTE_CS1 = 20;
+static const byte NOTE_D1 = 21;
+static const byte NOTE_EB1 = 22;
+static const byte NOTE_DS1 = 23;
+static const byte NOTE_E1 = 24;
+static const byte NOTE_F1 = 25;
+static const byte NOTE_GB1 = 26;
+static const byte NOTE_FS1 = 27;
+static const byte NOTE_G1 = 28;
+static const byte NOTE_AB1 = 29;
+static const byte NOTE_GS1 = 30;
+static const byte NOTE_A1 = 31;
+static const byte NOTE_BB1 = 32;
+static const byte NOTE_AS1 = 33;
+static const byte NOTE_B1 = 34;
+static const byte NOTE_C2 = 35;
+static const byte NOTE_DB2 = 36;
+static const byte NOTE_CS2 = 37;
+static const byte NOTE_D2 = 38;
+static const byte NOTE_EB2 = 39;
+static const byte NOTE_DS2 = 40;
+static const byte NOTE_E2 = 41;
+static const byte NOTE_F2 = 42;
+static const byte NOTE_GB2 = 43;
+static const byte NOTE_FS2 = 44;
+static const byte NOTE_G2 = 45;
+static const byte NOTE_AB2 = 46;
+static const byte NOTE_GS2 = 47;
+static const byte NOTE_A2 = 48;
+static const byte NOTE_BB2 = 49;
+static const byte NOTE_AS2 = 50;
+static const byte NOTE_B2 = 51;
+static const byte NOTE_C3 = 52;
+static const byte NOTE_DB3 = 53;
+static const byte NOTE_CS3 = 54;
+static const byte NOTE_D3 = 55;
+static const byte NOTE_EB3 = 56;
+static const byte NOTE_DS3 = 57;
+static const byte NOTE_E3 = 58;
+static const byte NOTE_F3 = 59;
+static const byte NOTE_GB3 = 60;
+static const byte NOTE_FS3 = 61;
+static const byte NOTE_G3 = 62;
+static const byte NOTE_AB3 = 63;
+static const byte NOTE_GS3 = 64;
+static const byte NOTE_A3 = 65;
+static const byte NOTE_BB3 = 66;
+static const byte NOTE_AS3 = 67;
+static const byte NOTE_B3 = 68;
+static const byte NOTE_C4 = 69;
+static const byte NOTE_DB4 = 70;
+static const byte NOTE_CS4 = 71;
+static const byte NOTE_D4 = 72;
+static const byte NOTE_EB4 = 73;
+static const byte NOTE_DS4 = 74;
+static const byte NOTE_E4 = 75;
+static const byte NOTE_F4 = 76;
+static const byte NOTE_GB4 = 77;
+static const byte NOTE_FS4 = 78;
+static const byte NOTE_G4 = 79;
+static const byte NOTE_AB4 = 80;
+static const byte NOTE_GS4 = 81;
+static const byte NOTE_A4 = 82;
+static const byte NOTE_BB4 = 83;
+static const byte NOTE_AS4 = 84;
+static const byte NOTE_B4 = 85;
+static const byte NOTE_C5 = 86;
+static const byte NOTE_DB5 = 87;
+static const byte NOTE_CS5 = 88;
+static const byte NOTE_D5 = 89;
+static const byte NOTE_EB5 = 90;
+static const byte NOTE_DS5 = 91;
+static const byte NOTE_E5 = 92;
+static const byte NOTE_F5 = 93;
+static const byte NOTE_GB5 = 94;
+static const byte NOTE_FS5 = 95;
+static const byte NOTE_G5 = 96;
+static const byte NOTE_AB5 = 97;
+static const byte NOTE_GS5 = 98;
+static const byte NOTE_A5 = 99;
+static const byte NOTE_BB5 = 100;
+static const byte NOTE_AS5 = 101;
+static const byte NOTE_B5 = 102;
+static const byte NOTE_C6 = 103;
+static const byte NOTE_DB6 = 104;
+static const byte NOTE_CS6 = 105;
+static const byte NOTE_D6 = 106;
+static const byte NOTE_EB6 = 107;
+static const byte NOTE_DS6 = 108;
+static const byte NOTE_E6 = 109;
+static const byte NOTE_F6 = 110;
+static const byte NOTE_GB6 = 111;
+static const byte NOTE_FS6 = 112;
+static const byte NOTE_G6 = 113;
+static const byte NOTE_AB6 = 114;
+static const byte NOTE_GS6 = 115;
+static const byte NOTE_A6 = 116;
+static const byte NOTE_BB6 = 117;
+static const byte NOTE_AS6 = 118;
+static const byte NOTE_B6 = 119;
+static const byte NOTE_C7 = 120;
+static const byte NOTE_DB7 = 121;
+static const byte NOTE_CS7 = 122;
+static const byte NOTE_D7 = 123;
+static const byte NOTE_EB7 = 124;
+static const byte NOTE_DS7 = 125;
+static const byte NOTE_E7 = 126;
+static const byte NOTE_F7 = 127;
+static const byte NOTE_GB7 = 128;
+static const byte NOTE_FS7 = 129;
+static const byte NOTE_G7 = 130;
+static const byte NOTE_AB7 = 131;
+static const byte NOTE_GS7 = 132;
+static const byte NOTE_A7 = 133;
+static const byte NOTE_BB7 = 134;
+static const byte NOTE_AS7 = 135;
+static const byte NOTE_B7 = 136;
+static const byte NOTE_C8 = 137;
+static const byte NOTE_DB8 = 138;
+static const byte NOTE_CS8 = 139;
+static const byte NOTE_D8 = 140;
+static const byte NOTE_EB8 = 141;
+static const byte NOTE_DS8 = 142;
+static const byte NOTE_E8 = 143;
+static const byte NOTE_F8 = 144;
+static const byte NOTE_GB8 = 145;
+static const byte NOTE_FS8 = 146;
+static const byte NOTE_G8 = 147;
+static const byte NOTE_AB8 = 148;
+static const byte NOTE_GS8 = 149;
+static const byte NOTE_A8 = 150;
+static const byte NOTE_BB8 = 151;
+static const byte NOTE_AS8 = 152;
+static const byte NOTE_B8 = 153;
+
+static const byte WHOLE = 0;
+static const byte HALF = 1;
+static const byte QUARTER = 2;
+static const byte EIGHTH = 3;
+static const byte SIXTH = 4;
+static const byte THIRD = 5;
+
+double note_durations[] = { 1.0, 0.5, 0.25, 0.125, 0.1667, 0.3333 };
+
+int note_frequencies[] = {
+  0, 16, 17, 17, 18, 19, 19, 21, 22, 23, 23, 245,
+  26, 26, 28, 29, 29, 31, 33, 35, 35, 37, 39, 39,
+  41, 44, 46, 46, 49, 52, 52, 55, 58, 58, 62, 65,
+  69, 69, 73, 78, 78, 82, 87, 93, 93, 98, 104, 104,
+  110, 117, 117, 123, 131, 139, 139, 147, 156, 156,
+  165, 175, 185, 185, 196, 208, 208, 220, 233, 233,
+  247, 262, 277, 277, 294, 311, 311, 330, 349, 370,
+  370, 392, 415, 415, 440, 466, 466, 494, 523, 554,
+  554, 587, 622, 622, 659, 698, 740, 740, 784, 831,
+  831, 880, 932, 932, 988, 1047, 1109, 1109, 1175,
+  1245, 1245, 1319, 1397, 1480, 1480, 1568, 1661,
+  1661, 1760, 1865, 1865, 1976, 2093, 2217, 2217,
+  2349, 2489, 2489, 2637, 2794, 2960, 2960, 3136,
+  3322, 3322, 3520, 3729, 3729, 3951, 4186, 4435,
+  4435, 4699, 4978, 4978, 5274, 5588, 5920, 5920,
+  6272, 6645, 6645, 7040, 7459, 7459, 7902
+};
+
+const byte notes[] = {
+  // comments correspond to page/line in sheet at
+  // http://www.mariopiano.com/mario-sheet-music-overworld-main-theme.html
+
+  // 1/1
+  NOTE_E7, NOTE_E7, SILENT, NOTE_E7,
+  SILENT, NOTE_C7, NOTE_E7, SILENT,
+  NOTE_G7, SILENT, SILENT,
+  NOTE_G6, SILENT, SILENT,
+
+  // 1/2
+  NOTE_C7, SILENT, SILENT, NOTE_G6,
+  SILENT, NOTE_E6, SILENT,
+  SILENT, NOTE_A6, SILENT, NOTE_B6,
+  SILENT, NOTE_BB6, NOTE_A6, SILENT,
+
+  // 1/3
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, SILENT, NOTE_F7, NOTE_G7,
+  SILENT, NOTE_E7, SILENT, NOTE_C7,
+  NOTE_D7, NOTE_B6, SILENT, SILENT,
+
+  // 1/4 (same as 1/2)
+  NOTE_C7, SILENT, SILENT, NOTE_G6,
+  SILENT, NOTE_E6, SILENT,
+  SILENT, NOTE_A6, SILENT, NOTE_B6,
+  SILENT, NOTE_BB6, NOTE_A6, SILENT,
+
+  // 1/5
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, SILENT, NOTE_F7, NOTE_G7,
+  SILENT, NOTE_E7, SILENT, NOTE_C7,
+  NOTE_D7, NOTE_B6, SILENT
+};
+
+const byte durations[] = {
+
+  // 1/1
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, HALF,
+  QUARTER, QUARTER, HALF,
+
+  // 1/2
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  HALF, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+
+  // 1/3
+  THIRD,   THIRD,   THIRD,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+
+  // 1/4
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  HALF, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+
+  // 1/5
+  THIRD,   THIRD,   THIRD,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, QUARTER, QUARTER,
+  QUARTER, QUARTER, HALF
+};
+
+void sound_game_over()
+{
+  tone(BUZZER_PIN, note_frequencies[NOTE_G4], 600);
+}
+
+void sound_pew()
+{
+  tone(BUZZER_PIN, note_frequencies[NOTE_G7], 50);
+}
+
+int index = 0;
+unsigned long previousMillis = 0;
+
+int numberOfNotes = sizeof(notes) / sizeof(notes[0]);
+double const BPM = 320.0; // beats per minute (quarter notes)
+double const PAUSE = 0.2; // portion of note should be silent before next note
+int whole_duration = (60.0f / BPM) * 4 * 1000;
+
+void play_melody()
+{
+  if (!MUSIC_ENABLED)
+    return;
+  
+  unsigned long currentMillis = millis();
+
+  if (index >= numberOfNotes) {
+    index = 0;
+  }
+
+  double duration = whole_duration * note_durations[durations[index]];
+
+  if (currentMillis >= (previousMillis + duration)) {
+    previousMillis = currentMillis;
+    index++;
+    
+    int freq = note_frequencies[notes[index]];
+    int playDuration = duration * (1 - PAUSE);
+
+    tone(BUZZER_PIN2, freq, playDuration);
+  }
+}
+
+void initSound()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUZZER_PIN2, OUTPUT);
+}
+
 
 /*
    Inputs
@@ -125,7 +431,6 @@ void draw()
     }
   }
 
-  debug("score is " + String(score) + "\n");
   set_score(score);
 }
 
@@ -140,7 +445,7 @@ void initDisplays()
 {
   for (int d = 0; d < lc_matrix.getDeviceCount(); d++) {
     lc_matrix.shutdown(d, false);
-    lc_matrix.setIntensity(d, 7);
+    lc_matrix.setIntensity(d, 8);
     lc_matrix.clearDisplay(d);
   }
 
@@ -157,7 +462,6 @@ void set_score(long num)
 
   byte digit = 0;
   while (true) {
-    Serial.println(num % 10);
     lc_segments.setDigit(0, digit++, num % 10, false);
     num /= 10;
     if (num <= 0) break;
@@ -171,14 +475,14 @@ void set_score(long num)
 
 void game_over()
 {
-  debug("game over");
+  debug("game over\n");
   game_running = false;
   game_over_screen();
 }
 
 void new_game()
 {
-  debug("new game");
+  debug("new game\n");
   frame = 0;
   ship_position = 3;
 
@@ -246,7 +550,7 @@ void detect_collisions()
       // detect shoting of asteroids
       long bullet_mask = bullets[row];
       if (bullet_mask & (1L << pos)) {
-        debug("bang\n");
+        debug("[explosion]\n");
         asteroids[row][i] = NUMBER_OF_COLS; // remove asteroid
         bullets[row] &= ~(1L << pos); // remove bullet
         score++;
@@ -266,8 +570,7 @@ void shoot()
   // new bullets start at column 2, to not overlap with the space ship
   bullets[ship_position] |= (1L << (NUMBER_OF_COLS - 3));
 
-  // pew pew
-  tone(BUZZER_PIN, 2960, 50);
+  sound_pew();
 }
 
 void flyDown()
@@ -332,18 +635,20 @@ long get_asteroid_mask(int row)
 void step_frame()
 {
   frame++;
-  frame %= 1000;
+  frame %= 100000;
   delay(FRAME_MS);
 }
 
 void setup()
 {
-  // setup displays
   initDisplays();
+  initSound();
 
   if (DEBUG) {
     Serial.begin(9600);
   }
+
+  debug("started\n");
 }
 
 void advance_game()
@@ -358,13 +663,12 @@ void advance_game()
 
   // view
   draw();
-
-  // wait
-  step_frame();
 }
 
 void game_over_screen()
 {
+  sound_game_over();
+
   long game[8] = {
     0B00000000000000000000000000000000L,
     0B00000111000011000010001000111000L,
@@ -416,11 +720,14 @@ void pre_game_screen()
 void loop()
 {
   check_buttons();
+  play_melody();
 
   if (!game_running) {
     pre_game_screen();
   } else {
     advance_game();
   }
+
+  step_frame();
 }
 
